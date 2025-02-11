a8cf8bdea75662839552b5664026f14c2c3d5782
Diego Martinez <diegommart2004@gmail.com>
2025-02-10 20:30:56 -0500

    added new project
    Demonstrates Interfaces: Vehicle requires startEngine method in its implementations
    
    Demonstrate Encapsulation: private fields, setters/getters/, speed validation
    
    Demonstrate Polymorphism: ElectricCar and GasCar are subclasses with overriding methods

---
+++ src/Buy_a_Pet.java
@@ -0,0 +1,2 @@
+public class Buy_a_Pet {
+}
--- src/Interfaces_and_Encapsulation.java
+++ src/Interfaces_and_Encapsulation.java
@@ -1,3 +1,84 @@
+interface Vehicle {
+    void startEngine(); // Abstract method to be implemented
+}
+
+// Car class implementing Vehicle
+class Car implements Vehicle {
+    private String model; // Private field for model
+    private int speed; // Private field for speed
+
+    // Constructor to initialize model and speed
+    public Car(String model, int speed) {
+        this.model = model;
+        setSpeed(speed); // Use setter for validation
+    }
+
+    // Getter for model
+    public String getModel() {
+        return model;
+    }
+
+    // Getter for speed
+    public int getSpeed() {
+        return speed;
+    }
+
+    // Setter for speed with validation (must be >= 0)
+    public void setSpeed(int speed) {
+        if (speed >= 0) {
+            this.speed = speed;
+        } else {
+            System.out.println("Error: Speed cannot be negative.");
+        }
+    }
+
+    // Implement startEngine method from Vehicle interface
+    @Override
+    public void startEngine() {
+        System.out.println("The engine of " + model + " is starting!");
+    }
+}
+
+// ElectricCar subclass extending Car
+class ElectricCar extends Car {
+    // Constructor for ElectricCar
+    public ElectricCar(String model, int speed) {
+        super(model, speed);
+    }
+
+    // Override startEngine for ElectricCar
+    @Override
+    public void startEngine() {
+        System.out.println("The electric engine of " + getModel() + " is starting!");
+    }
+}
+
+// GasCar subclass extending Car
+class GasCar extends Car {
+    // Constructor for GasCar
+    public GasCar(String model, int speed) {
+        super(model, speed);
+    }
+
+    // Override startEngine for GasCar
+    @Override
+    public void startEngine() {
+        System.out.println("The gas engine of " + getModel() + " is starting!");
+    }
+}
+
 public class Interfaces_and_Encapsulation {
-    
+    public static void main(String[] args) {
+        // Create instances of ElectricCar and GasCar
+        Car myElectricCar = new ElectricCar("Tesla Model S", 0);
+        Car myGasCar = new GasCar("Ford Mustang", 0);
+
+        // Set speed using setter
+        myElectricCar.setSpeed(60);
+        myGasCar.setSpeed(80);
+
+        // Call startEngine() for both cars
+        myElectricCar.startEngine();
+        myGasCar.startEngine();
+    }
 }
